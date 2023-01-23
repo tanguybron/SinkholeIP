73020114edef075ad5d59ed7d11f834f0e6c9318
Tanguy BRON <tanguy.bron@gmail.com>
2023-01-23 12:54:07 +0100

    import theme

--- source/conf.py
+++ source/conf.py
@@ -27,8 +27,8 @@ author = 'Tanguy BRON'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['myst_parser',
+        'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -48,12 +48,13 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'prev_next_buttons_location': 'bottom',
+    'sticky_navigation': True,
+    'navigation_depth': -1,
+    'includehidden': True,
+}
 html_static_path = ['_static']
+html_favicon = 'favicon.ico'
